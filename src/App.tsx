0b610760e0d3f8390251a6ebc6773bf6bfef473f
zeeshan_akram <zeeshan.akram@invozone.com>
2021-02-08 19:05:10 +0500

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import Routes from "./Routes";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./Redux/Actions";
 const App = () => {
-  // <Routes />
-  // const counter = useSelector((state) => state.counter);
-  // const disPatch = useDispatch();
-  // Home {counter}
-  // <button onClick={() => disPatch(increment())}>+</button>
-  // <button onClick={() => disPatch(decrement())}>-</button>
   return (
     <div>
       <Routes />
